149d382003a52e9fa2b8c921757ace296ee804c1
Steven Harlan <stevenharlan@gmail.com>
2022-01-14 17:24:36 -0800

    add test cov to action

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -3,11 +3,11 @@ echo "Hello World"
 
 curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import # One-time step
 
-curl -Os https://uploader.codecov.io/latest/macos/codecov
+curl -Os https://uploader.codecov.io/latest/linux/codecov
 
-curl -Os https://uploader.codecov.io/latest/macos/codecov.SHA256SUM
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
 
-curl -Os https://uploader.codecov.io/latest/macos/codecov.SHA256SUM.sig
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
 
 gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
 
