dd54e45a6835eea458d62fbc058ddcf0bcbd6715
Steven Harlan <stevenharlan@gmail.com>
2022-01-14 17:57:28 -0800

    add test to show branch uncovered

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, {sayHi } from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('say Hi says hi by default', () => {
+  const result = sayHi();
+  expect(result).toEqual('Hi');
+})
